1d90b9a281ce6ac41317a29373dc062b0a997b4b
pr4m1 <p.r4m1r@gmail.com>
2023-06-23 03:05:19 +0200

    Vuelve responsive la navbar

--- src/Configuration.jsx
+++ src/Configuration.jsx
@@ -1,7 +1,7 @@
 export const availableTimeSlots=[5,15,30,45,60,90,240];
 export const hours = ["00","01","02","03","04","05","06","07","08","09","10","11","12","13","14","15","16","17","18","19","20","21","22","23"]
-export const apiUrl = "http://localhost:8080/";
-//export const apiUrl = "http://192.168.1.130:8080/";
+//export const apiUrl = "http://localhost:8080/";
+export const apiUrl = "http://192.168.1.130:8080/";
 export const MANAGER_TYPE="manager";
 export const LAB_TYPE="lab"; 
 export const BOOKING_TYPE="booking";
--- src/structure/Header.jsx
+++ src/structure/Header.jsx
@@ -1,31 +1,48 @@
+import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import { IconMenu2,IconX } from '@tabler/icons-react';
 import '../style-components/Header.css';
 import bookLabIcon from '../assets/con_punto-removebg-preview.png';
 
-function Header(){
-    return(
-      <header>
-        <div id='container-header-left'>
-          <NavLink to='/' id='container-logo-titulo'>
-            <img src={bookLabIcon} alt='BookLab icon' />
-            <h1>BookLab</h1>
-          </NavLink>
-          <nav className='header-routes'>
-            <ul className='header-routes-list'>
-              <li><NavLink className='link-menu' to='/bookings'>Bookings</NavLink></li>
-              <li><NavLink className='link-menu' to='/labs'>Labs</NavLink></li>
-              <li><NavLink className='link-menu' to='/managers'>Managers</NavLink></li>
-            </ul>
-          </nav>
-        </div>
-        <div id='container-header-right'>
-          <nav className='header-login-register'>
-              <NavLink className='link-login' to='/'>Log In</NavLink>
-              <NavLink className='link-register' to='/'>Sign Up</NavLink>
-          </nav>
-        </div>
+function Header() {
+  const [showMenu, setShowMenu] = useState(false);
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
+  const handleResize = () => {
+    if(window.innerWidth > 900 && showMenu){
+      setShowMenu(false);
+    }
+  };
+  window.addEventListener('resize', handleResize);
+
+
+  return (
+    <header>
+      <NavLink to='/' id='container-logo-title'>
+        <img src={bookLabIcon} alt='BookLab icon' />
+        <h1>BookLab</h1>
+      </NavLink>
+      <nav className={`header-routes ${showMenu ? 'show' : ''}`}>
+        <ul className="header-routes-list">
+          <li><NavLink className="link-menu" to='/bookings'>Bookings</NavLink></li>
+          <li><NavLink className="link-menu" to='/labs'>Labs</NavLink></li>
+          <li><NavLink className="link-menu" to='/managers'>Managers</NavLink></li>
+        </ul>
+        <ul className="header-login-register">
+          <li><NavLink className={showMenu ? "link-menu" : "link-login"} to='/'>Log In</NavLink></li>
+          <li><NavLink className={showMenu ? "link-menu" : "link-register"} to='/'>Sign Up</NavLink></li>
+        </ul>
+      </nav>
+      <div className="icon-burger" onClick={toggleMenu}>
+          {showMenu?
+            <IconX size={30} />
+            :
+            <IconMenu2 size={30} />}
+      </div>
     </header>
-    );
+  );
 }
 
 export default Header;
